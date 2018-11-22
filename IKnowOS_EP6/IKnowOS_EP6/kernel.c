9d5268c5aad7e8847b5cbd5f0bc74572a8a0bbb6
Salim Shadman Ankur <31352360+Ankur-014@users.noreply.github.com>
2018-11-22 19:14:22 +0600

    Files on OS

---
+++ IKnowOS_EP6/IKnowOS_EP6/kernel.c
@@ -0,0 +1,33 @@
+#include "include/kb.h"
+#include "include/isr.h"
+#include "include/idt.h"
+kmain()
+{
+       isr_install();
+       clearScreen();
+       
+       print("Hi and Welcome to NIDOS operating system\nPlease enter a command");
+       printch('\n');
+       print("NIDOS> ");
+       while (1)
+       {
+                string ch = readStr();
+                print("\n");
+                print(ch);
+               /* if(strEql(ch,"cmd"))
+                {
+                        print("\nYou are allready in cmd\n");
+                }
+                else if(strEql(ch,"clear"))
+                {
+                        clearScreen();
+                        print("NIDOS> ");
+                }
+                
+                else
+                {
+                        print("Bad command!");
+                } */       
+       }
+        
+}
---
+++ IKnowOS_EP7/IKnowOS_EP7/README.txt
@@ -0,0 +1,4 @@
+You can visit The Video tutorial Linked to this archive at https://www.youtube.com/watch?v=CGkmfBHEODE
+This Archive has been uploaded by Nidhal Abidi From IKOW
+Please visit Iknow @ www.youtube.com/iknowbrain
+Thank you for your support :)
