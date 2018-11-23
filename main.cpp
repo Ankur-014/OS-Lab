9aa3c1040f5759344bb6714bcf92cc91805c7a44
Salim Shadman Ankur <31352360+Ankur-014@users.noreply.github.com>
2018-11-23 12:05:50 +0600

    Bankers Algorithm Lab

--- main.cpp
+++ main.cpp
@@ -1,76 +1,120 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using lli=long long int;
 using namespace std;
-
 
 int main()
 {
-    int r,p;
-    cout << "Enter the no. of processes" << endl;
-    cin>>p;
-    cout << "Enter the no. of resources" << endl;
-    cin>>r;
-
-    int allocated[p][r];
-    int requested[p][r];
-    int output[p][p];
-    for(int i=0;i<p;i++){
-        for(int j=0;j<p;j++){
-            output[i][j]=0;
+    lli need[100][100]={0};
+    lli available[100]={0};
+    lli allocated[100][100]={0};
+    cout<<"Enter the need for p0,p1,p2,p3:"<<endl;
+    for (lli i=0;i<4;i++)
+    {
+        for(lli j=0;j<5;j++)
+        {
+            cin>>need[i][j];
         }
     }
-    ///Allocated Matrix
-    cout<<"Enter the allocated matrix: "<<endl;
-    for(int i=0;i<p;i++){
-        for(int j=0;j<r;j++){
+    cout<<"Enter the Available for A,B,C,D,E:"<<endl;
+    for(lli i=0;i<5;i++)
+    {
+        cin>>available[i];
+    }
+    cout<<"Enter the allocated for p0,p1,p2,p3:"<<endl;
+     for (lli i=0;i<4;i++)
+    {
+        for(lli j=0;j<5;j++)
+        {
             cin>>allocated[i][j];
         }
     }
-    /*
-    0 0 1 0 0 1
-    0 1 0 0 0 0
-    0 0 0 0 1 0
-    0 0 0 1 0 0
-    1 0 0 0 0 0
+    lli indicator=0;
 
-    */
-    cout<<"Enter the requested matrix: "<<endl;
-    ///Requested Matrix
-    for(int i=0;i<p;i++){
-        for(int j=0;j<r;j++){
-            cin>>requested[i][j];
-        }
-    }
-
-    /*
-    0 0 0 0 0 0
-    0 0 0 0 0 1
-    1 0 0 1 0 0
-    0 0 0 0 1 1
-    0 1 0 1 0 0
-
-    */
-    for(int i=0;i<p;i++){
-        for(int j=0;j<r;j++){
-            if(allocated[i][j]==1)
+    while(1){
+        indicator=0;
+        for(lli i=0;i<4;i++)
+        {
+            lli check=0;
+            for(lli j=0;j<5;j++)
             {
-                for(int k=0;k<p;k++){
-                    if(requested[k][j]==1){
-                        output[k][i]=1;
-                        cout<<k<<" depends on "<<i<<endl;
-                    }
+                if(available[j]-need[i][j]<0)
+                {
+                    cout<<"p"<<i<<" will not run "<<endl;
+                    check=1;
+                    indicator=1;
+                    break;
+                }
 
+            }
+            if(check==0)
+            {
+                cout<<"p"<<i<<" will run "<<endl;
+                for(lli j=0;j<5;j++)
+                {
+                    available[j]+=allocated[i][j];
+                    allocated[i][j]=0;
                 }
             }
         }
+        if(indicator==0)
+            break;
+            cout<<endl;
     }
-    /*cout<<"Output: "<<endl;
-    for(int i=0;i<p;i++){
-        for(int j=0;j<p;j++){
-            cout<<output[i][j]<<" ";
+        cout<<"Now available : "<<endl;
+        for(lli j=0;j<5;j++)
+        {
+            cout<<available[j]<<" ";
         }
         cout<<endl;
-    }
-    */
+        cout<<"Enter number of input process : "<<endl;
+        lli np;
+        cin>>np;
+        cout<<"Enter needed resource :"<<endl;
+        for(lli i=0;i<np;i++)
+        {
+            for(lli j=0;j<5;j++)
+            {
+                cin>>need[i][j];
+            }
+
+        }
+
+        for(lli i=0;i<np;i++)
+        {
+            lli check=0;
+            for(lli j=0;j<5;j++)
+            {
+                if(available[j]-need[i][j]<0)
+                {
+                    cout<<"p"<<i<<" will not run "<<endl;
+                    check=1;
+                    indicator=1;
+                    break;
+                }
+
+            }
+            if(check==0)
+            {
+                cout<<"p"<<i<<" will run "<<endl;
+                for(lli j=0;j<5;j++)
+                {
+                    available[j]+=allocated[i][j];
+                    allocated[i][j]=0;
+                }
+            }
+        }
+
+        cout<<"Now available : "<<endl;
+        for(lli j=0;j<5;j++)
+        {
+            cout<<available[j]<<" ";
+        }
+
+
+
+
+
+
+
     return 0;
 }
