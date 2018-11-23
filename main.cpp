b8f7fdc2cb3d07a1360cf2d09ec9cf73a559c63f
Salim Shadman Ankur <31352360+Ankur-014@users.noreply.github.com>
2018-11-23 12:07:46 +0600

    Preemtive SJFS

--- main.cpp
+++ main.cpp
@@ -1,120 +1,166 @@
 #include <bits/stdc++.h>
-using lli=long long int;
 using namespace std;
-
+using lli=long long int;
+using d=double;
+priority_queue<lli>pq,current_process,tmp_process;
 int main()
 {
-    lli need[100][100]={0};
-    lli available[100]={0};
-    lli allocated[100][100]={0};
-    cout<<"Enter the need for p0,p1,p2,p3:"<<endl;
-    for (lli i=0;i<4;i++)
+    lli process[100]={-1},arrival[100]={-1},burst[100]={-1},completion[100]={0},pro,size1,brst[100]={-1};
+    d turnaround=0,waiting=0;
+    lli maximum=0,complete=0,min_b_process,n,indi=0;
+    cout<<"Enter the number of process : "<<endl;
+    cin>>n;
+    for(lli i=0;i<n;i++)
     {
-        for(lli j=0;j<5;j++)
+        cout<<"Enter process number,arrival time and burst time of that process :"<<endl;
+        cin>>process[i]>>arrival[i]>>burst[i];
+        pq.push(-arrival[i]);
+        brst[i]=burst[i];
+    }
+    cout<<endl;
+    for(lli i=0;i<n;i++)
+    {
+        maximum=max(arrival[i],maximum);
+    }
+    for(lli i=0;i<=maximum;i++)
+    {
+        if(current_process.empty())
         {
-            cin>>need[i][j];
+            ++complete;
+            indi=1;
         }
-    }
-    cout<<"Enter the Available for A,B,C,D,E:"<<endl;
-    for(lli i=0;i<5;i++)
-    {
-        cin>>available[i];
-    }
-    cout<<"Enter the allocated for p0,p1,p2,p3:"<<endl;
-     for (lli i=0;i<4;i++)
-    {
-        for(lli j=0;j<5;j++)
+        if(pq.empty()==false)
         {
-            cin>>allocated[i][j];
-        }
-    }
-    lli indicator=0;
-
-    while(1){
-        indicator=0;
-        for(lli i=0;i<4;i++)
+        if((-pq.top())==i)
         {
-            lli check=0;
-            for(lli j=0;j<5;j++)
+            for(lli k=0;k<n;k++)
             {
-                if(available[j]-need[i][j]<0)
+                if((-pq.top())==arrival[k])
                 {
-                    cout<<"p"<<i<<" will not run "<<endl;
-                    check=1;
-                    indicator=1;
+                    pro=k;
                     break;
                 }
-
             }
-            if(check==0)
+            pq.pop();
+            current_process.push(pro);
+        }
+        lli ttt=current_process.size();
+        if(current_process.empty()==false)
+        {
+            if(indi==1)
             {
-                cout<<"p"<<i<<" will run "<<endl;
-                for(lli j=0;j<5;j++)
+                --complete;
+                indi=0;
+            }
+            lli min_b_t=9999;
+            for(lli test=0;test<ttt;test++)
+            {
+                min_b_t=min(min_b_t,burst[current_process.top()]);
+                tmp_process.push(current_process.top());
+                current_process.pop();
+            }
+            current_process=tmp_process;
+            lli ccc=current_process.size();
+            while(tmp_process.empty()==false)
+            {
+                tmp_process.pop();
+            }
+            for(lli mn=0;mn<n;mn++)
+            {
+                if(min_b_t==burst[mn])
                 {
-                    available[j]+=allocated[i][j];
-                    allocated[i][j]=0;
-                }
-            }
-        }
-        if(indicator==0)
-            break;
-            cout<<endl;
-    }
-        cout<<"Now available : "<<endl;
-        for(lli j=0;j<5;j++)
-        {
-            cout<<available[j]<<" ";
-        }
-        cout<<endl;
-        cout<<"Enter number of input process : "<<endl;
-        lli np;
-        cin>>np;
-        cout<<"Enter needed resource :"<<endl;
-        for(lli i=0;i<np;i++)
-        {
-            for(lli j=0;j<5;j++)
-            {
-                cin>>need[i][j];
-            }
-
-        }
-
-        for(lli i=0;i<np;i++)
-        {
-            lli check=0;
-            for(lli j=0;j<5;j++)
-            {
-                if(available[j]-need[i][j]<0)
-                {
-                    cout<<"p"<<i<<" will not run "<<endl;
-                    check=1;
-                    indicator=1;
+                    min_b_process=mn;
                     break;
                 }
-
             }
-            if(check==0)
+            burst[min_b_process]--;
+            completion[min_b_process]=++complete;
+            for(lli test=0;test<ccc;test++)
             {
-                cout<<"p"<<i<<" will run "<<endl;
-                for(lli j=0;j<5;j++)
+                if(burst[current_process.top()]>0)
                 {
-                    available[j]+=allocated[i][j];
-                    allocated[i][j]=0;
+                    tmp_process.push(current_process.top());
+                    current_process.pop();
+                }
+                else
+                {
+                    current_process.pop();
                 }
             }
+            current_process=tmp_process;
+            lli sss=current_process.size();
+            for(lli tp=0;tp<tmp_process.size();tp++)
+            {
+                tmp_process.pop();
+            }
         }
 
-        cout<<"Now available : "<<endl;
-        for(lli j=0;j<5;j++)
+        }
+    }
+    while(current_process.empty()==false)
+    {
+         if(current_process.empty()==false)
         {
-            cout<<available[j]<<" ";
+            lli min_b_t=9999;
+            lli ttt=current_process.size();
+            for(lli test=0;test<ttt;test++)
+            {
+                min_b_t=min(min_b_t,burst[current_process.top()]);
+                tmp_process.push(current_process.top());
+                current_process.pop();
+            }
+            current_process=tmp_process;
+            lli ccc=current_process.size();
+            while(tmp_process.empty()==false)
+            {
+                tmp_process.pop();
+            }
+            for(lli mn=0;mn<n;mn++)
+            {
+                if(min_b_t==burst[mn])
+                {
+                    min_b_process=mn;
+                    break;
+                }
+            }
+            burst[min_b_process]--;
+            completion[min_b_process]=++complete;
+            for(lli test=0;test<ccc;test++)
+            {
+                if(burst[current_process.top()]>0)
+                {
+                    tmp_process.push(current_process.top());
+                    current_process.pop();
+                }
+                else
+                {
+                    current_process.pop();
+                }
+            }
+            current_process=tmp_process;
+            lli sss=current_process.size();
+            for(lli tp=0;tp<tmp_process.size();tp++)
+            {
+                tmp_process.pop();
+            }
         }
+    }
+    for(lli kp=0;kp<n;kp++)
+    {
+        cout<<completion[kp]<<endl;
+    }
 
-
-
-
-
-
-
+    for(lli kp=0;kp<n;kp++)
+    {
+        cout<<"Turnaround time p"<<process[kp]<<" : "<<completion[kp]-arrival[kp]<<endl;
+        turnaround+=(completion[kp]-arrival[kp]);
+    }
+    for(lli kp=0;kp<n;kp++)
+    {
+        cout<<"Waiting time p"<<process[kp]<<" : "<<completion[kp]-(arrival[kp]+brst[kp])<<endl;
+        waiting+=(completion[kp]-(arrival[kp]+brst[kp]));
+    }
+    cout<<"Average turnaround time : "<<turnaround/n<<endl;
+     cout<<"Average waiting time : "<<waiting/n<<endl;
     return 0;
 }
